3d080d10f2c364d0b3af228c7cf5153293b112f5
Johan <56638242+JohanBTB@users.noreply.github.com>
2022-11-07 17:44:32 -0500

    Correcion: La correcta es DeLaLuna2.html

--- js/carruselDeLaLuna.js
+++ js/carruselDeLaLuna.js
@@ -2,20 +2,22 @@ const d = document;
 
 function carruselLuna(){
     const $btnNext = d.querySelector('.next'), $btnPrev = d.querySelector('.prev');
-    const $slides = d.querySelectorAll('.slider-slide'), $balls = d.querySelectorAll('.ball')
+    const $slides = d.querySelectorAll('.slider-slide'); 
     for(let a = 0; a<$slides.length; a++){
-        let ball = `<a class="ball button" id="${a}" href="#"></a>`
+        let ball = `<a class="ball button ${ a==0? 'active': ''}" id="${a}" href="#"></a>`
         $btnNext.insertAdjacentHTML('beforebegin', ball);
-       
     }
+    const $balls = d.querySelectorAll('.ball');
     let pos =0;
     d.addEventListener('click', e=>{
         
         if(e.target == $btnNext){
             e.preventDefault();
             $slides[pos].classList.remove('active');
+            $balls[pos].classList.remove('active');
             pos== 3? pos = 0: pos+=1;
             $slides[pos].classList.add('active');
+            $balls[pos].classList.add('active');
         }
     });
     d.addEventListener('click', e=>{
@@ -23,8 +25,10 @@ function carruselLuna(){
         if(e.target == $btnPrev){
             e.preventDefault();
             $slides[pos].classList.remove('active');
+            $balls[pos].classList.remove('active');
             pos== 0? pos = 3: pos-=1;
             $slides[pos].classList.add('active');
+            $balls[pos].classList.add('active');
             console.log(pos);
         }
     });
@@ -33,8 +37,10 @@ function carruselLuna(){
         if(e.target.matches('.ball')){
             e.preventDefault();
             $slides[pos].classList.remove('active');
+            $balls[pos].classList.remove('active');
             pos = parseInt(e.target.id);
             $slides[pos].classList.add('active');
+            $balls[pos].classList.add('active');
         }
         
     });
